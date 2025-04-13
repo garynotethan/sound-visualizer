15022cd55de641acd7574d3f254e70b00db54487
garynotethan <huangethan93@gmail.com>
2025-04-12 19:05:14 -0700

    feat: main now runs the program

--- main.py
+++ main.py
@@ -1,24 +1,7 @@
-import pygame
-
+import os
 
 def main():
-
-    running = True 
-    screen = pygame.display.set_mode((1280, 720))
-    clock = pygame.time.Clock()
-    fps = 30
-    pygame.display.set_caption('Sound Visualizer')
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-
-        clock.tick(fps)
-
-    #game is exited
-    pygame.quit()
-
+    os.system(f'python src/gui.py')
 
 if __name__ == "__main__":
     main()
